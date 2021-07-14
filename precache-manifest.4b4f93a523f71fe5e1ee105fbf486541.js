d073ff6cab258af6ef8bde2a92a4a07f56b4f428
mgbarois <mgbarois@gmail.com>
2021-07-14 11:58:02 +0200

    Updates

---
+++ precache-manifest.4b4f93a523f71fe5e1ee105fbf486541.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2786bb40e26fffd18ffa4a66e616df80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d7b853afaa4c7ad3b5",
+    "url": "/static/css/2.dc1a0cbc.chunk.css"
+  },
+  {
+    "revision": "5003ddd5148aa220fb6f",
+    "url": "/static/css/main.1a8f0ebb.chunk.css"
+  },
+  {
+    "revision": "34d7b853afaa4c7ad3b5",
+    "url": "/static/js/2.a0137c86.chunk.js"
+  },
+  {
+    "revision": "08be2e9387cdedd163b26ec41e315091",
+    "url": "/static/js/2.a0137c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5003ddd5148aa220fb6f",
+    "url": "/static/js/main.e37e435c.chunk.js"
+  },
+  {
+    "revision": "ed711f650794b218d9c3",
+    "url": "/static/js/runtime-main.335874f4.js"
+  },
+  {
+    "revision": "4bd5eae1c1bdc927dbae8b0f0fb1dba3",
+    "url": "/static/media/NADA_logo.4bd5eae1.png"
+  },
+  {
+    "revision": "478fe445f9ca26036c5af035e6fdfc8d",
+    "url": "/static/media/acolad.478fe445.webp"
+  },
+  {
+    "revision": "5de9b5adb1b415af03021b5ecfb9afb0",
+    "url": "/static/media/aron-visuals-BXOXnQ26B7o-unsplash.5de9b5ad.jpg"
+  },
+  {
+    "revision": "b6b27508ce7b6421cd89082eca97630a",
+    "url": "/static/media/art_gray.b6b27508.png"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "275b95619c5575c439b2c5c5d4443c36",
+    "url": "/static/media/berlitz_logo_gray.275b9561.png"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "7ad2306455f488693bdc0d0500d92233",
+    "url": "/static/media/blogdown.7ad23064.png"
+  },
+  {
+    "revision": "4588517576d0de1e8f30733593f436ea",
+    "url": "/static/media/bootstrap-brands.45885175.svg"
+  },
+  {
+    "revision": "5ec348e0407416f9e6f1632befff1219",
+    "url": "/static/media/bruno-nascimento-PHIgYUGQPvU-unsplash.5ec348e0.jpg"
+  },
+  {
+    "revision": "fc225f4848691499990003cd20a93435",
+    "url": "/static/media/chacunsoncourt_logo.fc225f48.jpg"
+  },
+  {
+    "revision": "9c707f28ca50aca2d1abe2167339cc89",
+    "url": "/static/media/chacunsoncourt_logo_gray.9c707f28.png"
+  },
+  {
+    "revision": "a7943ad7b6c5165ff7187c0899ff28a7",
+    "url": "/static/media/csharp_crop.a7943ad7.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "c540cd041dd9493463fd1d3394e34092",
+    "url": "/static/media/fishes.c540cd04.jpg"
+  },
+  {
+    "revision": "e34f524343383d3a28ac3184acef21f0",
+    "url": "/static/media/globalese_logo.e34f5243.png"
+  },
+  {
+    "revision": "757226a0b1f5eec1fa05343cd90386d9",
+    "url": "/static/media/jellyfish.757226a0.jpg"
+  },
+  {
+    "revision": "700dc7013cff1dbac30cc467f1c1e2eb",
+    "url": "/static/media/jonathan-borba-3o5oUjrD90w-unsplash_grad.700dc701.png"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "3750e421e9f4f4bd064c5db3d22aaa3a",
+    "url": "/static/media/lang_gray.3750e421.png"
+  },
+  {
+    "revision": "82e56afffc55972a42a45fe5d8b5f7f2",
+    "url": "/static/media/languages_white.82e56aff.png"
+  },
+  {
+    "revision": "bdbe96d5375fa3859975b9f459d7a2f3",
+    "url": "/static/media/loca_eng_gray.bdbe96d5.png"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "c9e47c0b8a464d90eb2ba27e246bb889",
+    "url": "/static/media/logo_circuit_white.c9e47c0b.png"
+  },
+  {
+    "revision": "a67f4dcd0ff93e1ae49301ea8de7cd57",
+    "url": "/static/media/markus-winkler-jid7TSoa02c-unsplash.a67f4dcd.jpg"
+  },
+  {
+    "revision": "c6f84c263e42951fe288844c0d68b1f9",
+    "url": "/static/media/master-caweb.c6f84c26.png"
+  },
+  {
+    "revision": "0d7ca8dd485e44be131ea3f8df4be040",
+    "url": "/static/media/me.0d7ca8dd.jpg"
+  },
+  {
+    "revision": "59dee6df505cc0760562fa2fce520603",
+    "url": "/static/media/memoq.59dee6df.svg"
+  },
+  {
+    "revision": "43ffc764d203ed503bdeaab0706796c6",
+    "url": "/static/media/moa (3).43ffc764.png"
+  },
+  {
+    "revision": "6c94cb6ba186970da64d0f586eba4412",
+    "url": "/static/media/moa_gray.6c94cb6b.png"
+  },
+  {
+    "revision": "2cd210fa7afe17f1b335b456641ecf75",
+    "url": "/static/media/mr-cup-fabien-barral-o6GEPQXnqMY-unsplash.2cd210fa.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "13a0c18ecb934a890812c772e9e969b0",
+    "url": "/static/media/okapi_logo.13a0c18e.png"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "bb5b6c10f3b48eac447b50cade9b05c1",
+    "url": "/static/media/regex_crop.bb5b6c10.png"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "e0bd1d33f774c5940eda52a227bf36f5",
+    "url": "/static/media/sdl_logo.e0bd1d33.png"
+  },
+  {
+    "revision": "5fdfaa0aacbaece2ffea3dc52bd2a203",
+    "url": "/static/media/smartkyc_logo_gray.5fdfaa0a.png"
+  },
+  {
+    "revision": "9279f6059d6146a1f8c00614867454ba",
+    "url": "/static/media/tcloc_logo_gray.9279f605.png"
+  },
+  {
+    "revision": "62f35441c7242b4eab243b76ea9df2f1",
+    "url": "/static/media/telelingua_logo.62f35441.png"
+  },
+  {
+    "revision": "b390dbf7f6112763bd359d216f021e46",
+    "url": "/static/media/telelingua_logo_gray.b390dbf7.png"
+  },
+  {
+    "revision": "7273863d8803d287afdf55de92147a7e",
+    "url": "/static/media/ubc_logo.7273863d.jpg"
+  },
+  {
+    "revision": "1aee5bbec76036fb3538b5c7cddb022c",
+    "url": "/static/media/ubc_logo_gray.1aee5bbe.png"
+  },
+  {
+    "revision": "29ad3e9944aeb2fc9f1596bb5113ce2a",
+    "url": "/static/media/web_gray.29ad3e99.png"
+  }
+]);
